f0d92024e6154c0dfd73667e3e954fbac42c008d
KimHyoJin <hyojin03131995@gmail.com>
2020-04-21 23:28:21 +0900

    Initialize the project

---
+++ src/test/java/com/hyojin/OneDayOneCommit/OneDayOneCommitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyojin.OneDayOneCommit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneDayOneCommitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
